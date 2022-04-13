085ac006e6ec19989efb8cf438b3aefbd98a19b7
dwcalade <dwcalade@yahoo.ca>
2022-04-13 21:23:52 +0000

    constants.js

---
+++ constants.js
@@ -0,0 +1,15 @@
+const IP = '10.0.2.15';
+const PORT = 50541;
+const UPKEY = "Move: up";
+const LEFTKEY = "Move: left";
+const DOWNKEY = "Move: down";
+const RIGHTKEY = "Move: right";
+
+module.exports = {
+  IP,
+  PORT,
+  UPKEY,
+  LEFTKEY,
+  DOWNKEY,
+  RIGHTKEY
+};
